15892d435c1d325617e8279bbe5dfd7f571c2176
Aniket Handa <atneik@gmail.com>
2014-03-08 01:01:10 -0800

    edit for L3G4200D 3-axis gyroscope

--- Arduino/Razor_AHRS/Sensors.ino
+++ Arduino/Razor_AHRS/Sensors.ino
@@ -5,7 +5,15 @@
 // Sensor I2C addresses
 #define ACCEL_ADDRESS ((int) 0x53) // 0x53 = 0xA6 / 2
 #define MAGN_ADDRESS  ((int) 0x1E) // 0x1E = 0x3C / 2
-#define GYRO_ADDRESS  ((int) 0x68) // 0x68 = 0xD0 / 2
+//#define GYRO_ADDRESS  ((int) 0x68) // 0x68 = 0xD0 / 2
+int L3G4200D_Address = 105; //I2C address of the L3G4200D
+int gyro_Scale = 2000;
+
+#define CTRL_REG1 0x20
+#define CTRL_REG2 0x21
+#define CTRL_REG3 0x22
+#define CTRL_REG4 0x23
+#define CTRL_REG5 0x24
 
 // Arduino backward compatibility macros
 #if ARDUINO >= 100
@@ -144,7 +152,7 @@ void Read_Magn()
     if (output_errors) Serial.println("!ERR: reading magnetometer");
   }
 }
-
+/*
 void Gyro_Init()
 {
   // Power up reset defaults
@@ -207,4 +215,74 @@ void Read_Gyro()
     num_gyro_errors++;
     if (output_errors) Serial.println("!ERR: reading gyroscope");
   }
+}
+*/
+
+void Gyro_Init(){
+  
+  // Enable x, y, z and turn off power down:
+  writeRegister(L3G4200D_Address, CTRL_REG1, 0b00001111);
+
+  // If you'd like to adjust/use the HPF, you can edit the line below to configure CTRL_REG2:
+  writeRegister(L3G4200D_Address, CTRL_REG2, 0b00000000);
+
+  // Configure CTRL_REG3 to generate data ready interrupt on INT2
+  // No interrupts used on INT1, if you'd like to configure INT1
+  // or INT2 otherwise, consult the datasheet:
+  writeRegister(L3G4200D_Address, CTRL_REG3, 0b00001000);
+
+  // CTRL_REG4 controls the full-scale range, among other things:
+
+  if(gyro_Scale == 250){
+    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00000000);
+  }else if(gyro_Scale == 500){
+    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00010000);
+  }else{
+    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00110000);
+  }
+
+  // CTRL_REG5 controls high-pass filtering of outputs, use it
+  // if you'd like:
+  writeRegister(L3G4200D_Address, CTRL_REG5, 0b00000000);
+  
+}
+
+
+// Reads x, y and z gyroscope registers
+void Read_Gyro(){
+  byte xMSB = readRegister(L3G4200D_Address, 0x29);
+  byte xLSB = readRegister(L3G4200D_Address, 0x28);
+  gyro[0] = ((xMSB << 8) | xLSB);
+
+  byte yMSB = readRegister(L3G4200D_Address, 0x2B);
+  byte yLSB = readRegister(L3G4200D_Address, 0x2A);
+  gyro[1] = ((yMSB << 8) | yLSB);
+
+  byte zMSB = readRegister(L3G4200D_Address, 0x2D);
+  byte zLSB = readRegister(L3G4200D_Address, 0x2C);
+  gyro[2] = ((zMSB << 8) | zLSB);
+}
+
+void writeRegister(int deviceAddress, byte address, byte val) {
+    Wire.beginTransmission(deviceAddress); // start transmission to device 
+    Wire.write(address);       // send register address
+    Wire.write(val);         // send value to write
+    Wire.endTransmission();     // end transmission
+}
+
+int readRegister(int deviceAddress, byte address){
+
+    int v;
+    Wire.beginTransmission(deviceAddress);
+    Wire.write(address); // register to read
+    Wire.endTransmission();
+
+    Wire.requestFrom(deviceAddress, 1); // read a byte
+
+    while(!Wire.available()) {
+        // waiting
+    }
+
+    v = Wire.read();
+    return v;
 }
